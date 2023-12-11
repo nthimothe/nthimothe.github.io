7a83fad558a469005f493d2dc145944ef7619dc4
Nathan Thimothe <nht1@williams.edu>
2023-12-10 17:46:25 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.08d9a58d5c2c7802a5ea7258666f8367.js"
+  "/precache-manifest.7a4dc5be39115b5085730c0177269d4f.js"
 );
 
 self.addEventListener('message', (event) => {
